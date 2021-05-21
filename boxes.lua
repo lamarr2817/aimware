aaccacf97ffed18d0e3b0e01ec6a719886776bc6
lamarr2817 <57584063+lamarr2817@users.noreply.github.com>
2021-05-21 17:09:14 -0400

    we do a little outline

--- boxes.lua
+++ boxes.lua
@@ -1,9 +1,9 @@
--- biggest shouts to trollface and scape
-
 local ref = gui.Reference("Visuals")
 local tab = gui.Tab(ref, "box", "MOAR BOX")
 local gbox = gui.Groupbox(tab, "Box Settings", 16, 16, 296, 1)
+local team = gui.Checkbox(gbox, "team", "Show on Teammates", 0)
 local edge = gui.Checkbox(gbox, "edge", "Box Edge", 1)
+local outline = gui.Checkbox(gbox, "outline", "Outline", 1)
 local edgeamt = gui.Slider(gbox, "edge.size", "Edge Size", 30, 1, 50)
 local edgecol = gui.ColorPicker(edge, "edge.color", "Edge Color", 255, 255, 255, 255)
 
@@ -14,7 +14,7 @@ local fillcolhp = gui.Checkbox(gbox, "fill.color.hp", "Color by HP", 0)
 
 callbacks.Register("DrawESP", function(esp)
     if not esp:GetEntity():IsAlive() then return end
-
+    if (esp:GetEntity():GetTeamNumber() == entities.GetLocalPlayer():GetTeamNumber()) and not team:GetValue() then return end
     local x, y, x2, y2 = esp:GetRect()
 
     if fill:GetValue() then
@@ -35,6 +35,28 @@ callbacks.Register("DrawESP", function(esp)
         end
     end
 
+    if outline:GetValue() then
+
+        local size = 1 - edgeamt:GetValue() * 0.01
+
+        draw.Color(0, 0, 0, 255)
+        draw.ShadowRect((x) + (x2 - x) * size, y2, x2, y2, 7)
+        draw.ShadowRect(x2, (y) + (y2 - y) * size, x2, y2, 7)
+
+
+        draw.ShadowRect(x, (y) + (y2 - y) * size, x, y2, 7)
+        draw.ShadowRect(x, y2, (x2) - (x2 - x) * size, y2, 7)
+
+
+        draw.ShadowRect(x, (y2) - (y2 - y) * size, x, y, 7)
+        draw.ShadowRect(x, y, (x2) - (x2 - x) * size, y, 7)
+
+
+        draw.ShadowRect((x) + (x2 - x) * size, y, x2, y, 7)
+        draw.ShadowRect(x2, (y2) - (y2 - y) * size, x2, y, 7)
+
+    end
+
     if edge:GetValue() then
 
         local size = 1 - edgeamt:GetValue() * 0.01
@@ -53,4 +75,6 @@ callbacks.Register("DrawESP", function(esp)
         draw.Line(x, y, (x2) - (x2 - x) * size, y)
         draw.Line(x2, (y2) - (y2 - y) * size, x2, y)
     end
+
+
 end)
